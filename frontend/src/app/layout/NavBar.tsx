1de3eebeb79158c29f7833162be3f199dda93c2d
amircherif-ac <amircherif-ac@hotmail.fr>
2025-01-13 20:00:05 -0500

    React Router, Navbar links, Product details page
    - React Router integration for seamless navigation.
    - Navbar links added for intuitive user interaction.
    - Product Details page implemented for detailed item views.

--- frontend/src/app/layout/NavBar.tsx
+++ frontend/src/app/layout/NavBar.tsx
@@ -1,10 +1,84 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  { title: "Catalog", path: "/catalog" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "Login", path: "/login" },
+  { title: "Register", path: "/register" },
+];
+
+const navStyles = {
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "#baecf9",
+  },
+};
 
 export default function NavBar() {
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6">SnowPro</Typography>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display='flex' alignItems='center'>
+          <img
+            src="../../../public/logo.svg"
+            alt="logo"
+            style={{ height: "2.8rem", filter: "invert(1)" }}
+          />
+          <Typography component={NavLink} to="/" variant="h6" sx={navStyles}>
+            SnowPro
+          </Typography>
+        </Box>
+
+        <Box>
+          <List sx={{ display: "flex" }}>
+            {midLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title} 
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        <Box display='flex' alignItems='center'>
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- frontend/src/features/catalog/Catalog.tsx
+++ frontend/src/features/catalog/Catalog.tsx
@@ -1,11 +1,17 @@
-import { Product } from "../../app/models/product";
+import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
+import { Product } from "../../app/models/product";
 
-type Props = {
-  products: Product[];
-};
+export default function Catalog() {
 
-export default function Catalog({products}: Props) {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+  
   return (
     <>
       <ProductList products={products} />
